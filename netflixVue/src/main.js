3ca71d17b97773cc06eccad5ab1f7d8ee128d6f1
Rogerio Pereira <rogerio@colmeiatecnologia.com.br>
2019-01-23 22:46:40 -0600

    Seção 1, Aula 36 - Modelo Simples de Rotas

--- netflixVue/src/main.js
+++ netflixVue/src/main.js
@@ -1,9 +1,27 @@
 import Vue from 'vue'
-import App from './App.vue'
+//import App from './App.vue'
 import VueResource from 'vue-resource'
 Vue.use(VueResource)
 
+import Home from './paginas/Home.vue'
+const Contato = {template: '<p>Pagina de Contato</p>'}
+const SemPagina = {template: '<p>Esta página não existe!</p>'}
+
+
+const rotas = {
+  '/': Home,
+  '/contato': Contato
+}
+
 new Vue({
   el: '#app',
-  render: h => h(App)
+  data:{
+    rotaAtual: window.location.pathname
+  },
+  computed:{
+    exibeComponente() {
+      return rotas[this.rotaAtual] || SemPagina
+    }
+  },
+  render(h) {return h(this.exibeComponente)}
 })
